b33f42202ea93a8cd3aacde6b9f137e0a6e3b58b
Hector <hectorllorca7@gmail.com>
2023-06-12 13:36:44 +0200

    first commit

---
+++ src/js/views/home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => (
+  <div className="text-center mt-5">
+    <h1>Hello Rigo!</h1>
+    <a href="#" className="btn btn-success">
+      If you see this green button, bootstrap is working
+    </a>
+  </div>
+);
