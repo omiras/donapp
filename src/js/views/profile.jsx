f2c88aba6acc2e7111fb92d8a0383f396fd41490
David Pérez <devrap99@gmail.com>
2023-06-13 14:21:34 +0200

    primera parte hecha

--- src/js/views/profile.jsx
+++ src/js/views/profile.jsx
@@ -1,4 +1,13 @@
+
 export const Profile = () => {
-    return ( <img src="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.webp?w=1600&h=1067"/>
+    return ( 
+        <><div className="avatar">
+            <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <img src="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.webp?w=1600&h=1067" />
+            </div>
+        </div>
+        <div>
+                <h2>NameTag</h2>
+            </div></>
     );
 }
