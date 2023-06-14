c50aac7d2ccf11f1d8f8bbc800d182917c62e0c0
Carmen <path4design@gmail.com>
2023-06-14 14:24:54 +0200

    update code

--- src/js/component/footer.jsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => (
-  <footer>
-    <p>
-      Made By Singulars 2023 - <a href="https://github.com/Singulars-2023-Front-End/donapp">GitHub</a>
-    </p>
-  </footer>
-);
--- src/js/layout.jsx
+++ src/js/layout.jsx
@@ -6,7 +6,6 @@ import { Newsletter } from "./views/newsletter";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
@@ -18,13 +17,12 @@ const Layout = () => {
     <div>
       <BrowserRouter basename={basename}>
         <ScrollToTop>
-          <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/newsletter" element={<Newsletter />} />
             <Route path="*" element={<h1>Not found!</h1>} />
           </Routes>
-          <Footer />
+          <Navbar />
         </ScrollToTop>
       </BrowserRouter>
     </div>
