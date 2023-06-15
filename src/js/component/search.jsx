f586623cec97843a042f34f2227f9740b5b8e1c2
Júlia Valls Moix <juliavalls20@gmail.com>
2023-06-15 14:03:53 +0200

    I think it's done (Oscar needed to check))

--- src/js/component/search.jsx
+++ src/js/component/search.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { HiOutlineSearch } from "react-icons/hi"
 
-const SearchInput = () => {
- return(
-    <>
-        <input type="search" placeholder="Buscar" className=" input input-bordered input-accent w-full" />
-    <button className="btn">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-    </button>
-    </>
- )
+const SearchInput = ({ value, onSearchChange }) => {
+    return (
+        <>
+            <div className="inline-block relative" >
+                <input id="search" name="search" value={value} onChange={onSearchChange} type="search" placeholder="Buscar" className="input input-bordered input-accent w-full relative pl-12" />
+                <svg className="absolute left-3 top-1/2 transform -translate-y-1/2" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" style={{ color: 'gray' }}>
+                    <path style={{ fill: 'currentcolor' }} d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
+            </div>
+        </>
+    )
 };
 
 export default SearchInput;
--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -1,20 +1,36 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import SearchInput from "../component/search";
+import { useState } from "react";
 
 const Home = () => {
   const { store, actions } = useContext(Context);
+  
+  const [search, setSearch] = useState('');
+  const [donations, setDonations] = useState([...store.donations]);
+
+  const handleFilter = (e) => {
+    
+    const keyword = e.target.value;
+
+    const filteredDonations = donations.filter(d => new RegExp(keyword, 'i').test(d.name));
+
+    setSearch(keyword);
+    setDonations(filteredDonations);
+
+  }
+
   return (
     <div>
-    <div className="flex justify-center align-center mb-3 gap-2">
-    <SearchInput/>
+    <div className="flex justify-center align-center mb-3 gap-2 flex-col">
+    <SearchInput value={search} onSearchChange={handleFilter} />
     </div>
 
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-4xl font-bold dark:text-white text-center">Donation List</h2>
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {store.donations.map((donation, index) => (
+          {donations.map((donation, index) => (
             <a href="#" className="group relative" key={index}>
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                 <img src={donation.imageURL} alt="..." className="image h-full w-full object-cover object-center group-hover:opacity-75" />
