554e479b1b03cc84e2328a385181a34fbc51516a
Oscar Miras <oscar.miras@gmail.com>
2023-06-20 12:05:48 +0200

    removed h2

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -30,7 +30,6 @@ const Home = () => {
 
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-4xl font-bold dark:text-white text-center">Donation List</h2>
 
           {donations.length > 0 ? (<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {donations.map((donation, index) => (
