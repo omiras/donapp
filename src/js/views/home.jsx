b970c02a049878c4d9570222301ce627db537b28
Júlia Valls Moix <juliavalls20@gmail.com>
2023-06-20 11:06:47 +0200

    Cat added

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -5,18 +5,18 @@ import { useState } from "react";
 
 const Home = () => {
   const { store, actions } = useContext(Context);
-  
+
   const [search, setSearch] = useState('');
   const [donations, setDonations] = useState([...store.donations]);
 
   const handleFilter = (e) => {
-    
+
     const keyword = e.target.value;
     const keywordRegex = new RegExp(keyword, 'i');
 
     const filteredDonations = store.donations.filter(d => keywordRegex.test(d.name) || keywordRegex.test(d.description));
-  
-  
+
+
     setSearch(keyword);
     setDonations(filteredDonations);
 
@@ -24,28 +24,29 @@ const Home = () => {
 
   return (
     <div>
-    <div className="flex justify-center align-center mb-3 gap-2 flex-col">
-    <SearchInput value={search} onSearchChange={handleFilter} />
-    </div>
-
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-4xl font-bold dark:text-white text-center">Donation List</h2>
-
-        {donations.length > 0 ? (<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {donations.map((donation, index) => (
-            <a href="#" className="group relative" key={index}>
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                <img src={donation.imageURL} alt="..." className="image h-full w-full object-cover object-center group-hover:opacity-75" />
-                <p className="absolute bottom-3 left-3 badge-lg badge">{donation.productStatus}</p>
-              </div>
-            </a>
-          ))}
-        </div>) : (<p className="text-center m-6">No hay artículos con la descripción proporcionada.</p>)}
-
-        
+      <div className="flex justify-center align-center mb-3 gap-2 flex-col">
+        <SearchInput value={search} onSearchChange={handleFilter} />
       </div>
-    </div>
+
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="text-4xl font-bold dark:text-white text-center">Donation List</h2>
+
+          {donations.length > 0 ? (<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {donations.map((donation, index) => (
+              <a href="#" className="group relative" key={index}>
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                  <img src={donation.imageURL} alt="..." className="image h-full w-full object-cover object-center group-hover:opacity-75" />
+                  <p className="absolute bottom-3 left-3 badge-lg badge">{donation.productStatus}</p>
+                </div>
+              </a>
+            ))}
+          </div>) : (<div className="flex flex-col items-center justify-center"> <p className="text-center m-6">No hay artículos con la descripción proporcionada.</p>
+            <img src="https://media.tenor.com/gK32v_OWs0kAAAAM/omg-cat.gif" alt="Not Found GIF" /> </div>)}
+
+
+        </div>
+      </div>
     </div>
   );
 
