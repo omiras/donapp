49973fda56c5c724a2a7e1b8ca4f82878e1ae07d
Franciellen <franciellen_gc@hotmail.com>
2023-06-23 12:15:25 +0200

    updade icon

--- src/js/views/profile.jsx
+++ src/js/views/profile.jsx
@@ -3,30 +3,37 @@ import { Context } from "../store/appContext";
 import StaticRating from '../component/StaticRating';
 import { useState } from 'react';
 import DonationList from '../component/donationList';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 export const Profile = () => {
 
     const { store, actions } = useContext(Context);
-    console.log(store)
+    console.log(store);
 
-    // TODO: Cambiar por el rating de base de datos
-    const [rating, setRating] = useState(1)
+    const handleLogout = () => {
+        // Lógica para cerrar sesión
+    };
+
+    // TODO: Cambiar por el rating de la base de datos
+    const [rating, setRating] = useState(1);
 
     const handleClick = (ratingValue) => {
-
-        setRating(ratingValue)
+        setRating(ratingValue);
         console.log(ratingValue);
-        // setRating(ratingValue)
-        // console.log(rating);
-    }
+    };
 
     const donations = store.donations.filter(d => d.user_id === store.user.id);
 
     return (
-        <div >
+        <div>
+            <div className="divider"></div>
+            <button className="btn" onClick={handleLogout}>
+                <FontAwesomeIcon icon={faSignOutAlt} /> Sign out
+            </button>
+
             <div className="flex justify-center items-center flex-col m-4 pt-4">
-                <img className='w-206px h-216px rounded-full ring ring-secondary ring-offset-secondary ring-offset-2' src={store.user.avatar_url} />
+                <img className='w-206px h-216px rounded-full ring ring-secondary ring-offset-secondary ring-offset-2' src={store.user.avatar_url} alt="Avatar" />
             </div>
             <div className="flex justify-center items-center flex-col m-4 pt-4">
                 <h2 className='text-3xl text-center text-primary'>{store.user.full_name}</h2>
@@ -34,25 +41,21 @@ export const Profile = () => {
             <div className="flex justify-center items-center flex-col m-4 mb-12 ml-1 pt-4 gap-10 h-28">
                 {/* Traer esto de la base de datos */}
                 <StaticRating rating={4} isEditable={false} />
-            </div >
+            </div>
             <div className="flex bg-secondary justify-evenly text-primary w-full text-center place-items-center h-52">
-
-                <div className="flex flex-col  ">
+                <div className="flex flex-col">
                     <span className='text-4xl'>{actions.getDonationCount()}</span>
-                    <span className='text-xl '>Donaciones</span>
+                    <span className='text-xl'>Donaciones</span>
                 </div>
-
                 <span className='h-24 border border-base-100'></span>
-
                 <div className="flex flex-col">
                     <span className='text-4xl'>234</span>
                     <span className='text-xl'>Likes</span>
                 </div>
-
             </div>
             <div>
                 <DonationList items={donations} />
             </div>
         </div>
     );
-}
+};
