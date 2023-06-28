ea149cf4ccb9e57ea22e1fdb21cb743cd1ae5667
javier aldana <jdad1701@gmail.com>
2023-06-28 14:06:50 +0200

    funcionalidades botones

--- src/js/views/SplashPage.jsx
+++ src/js/views/SplashPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import background from "../../assets/img/dona+_splash.png"
+import { Link } from "react-router-dom";
 
 
 
@@ -13,19 +14,25 @@ function SplashPage() {
     return (
     
     <div style={{
+        backgroundColor:"#e0fdf8",
         backgroundImage: `url(${background})`,
-        backgroundSize: "cover",
+        backgroundSize: "contain",
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
         minHeight: "100vh",
+        
         }} className="flex justify-around items-end"> 
         {/* <img></img> */}
             <div className="flex">
+                <Link to="/contact">
                 <button className="btn">¿Quienes somos?</button>
+                </Link>
                 </div>
                 <nav></nav>
                 <div className="flex">
+                <Link to="/">
                 <button className="btn">¡Empieza a Donar+!</button>
+                </Link>
             </div>
     </div>
     
