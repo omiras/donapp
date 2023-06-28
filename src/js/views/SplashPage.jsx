60ecf8f74f4e0dd7e5f57ba3654776409f00faf9
Cynox336 <cynox336@gmail.com>
2023-06-28 14:19:16 +0200

    Update style button 14:19 28/06

--- src/js/views/SplashPage.jsx
+++ src/js/views/SplashPage.jsx
@@ -3,11 +3,9 @@ import background from "../../assets/img/dona+_splash.png";
 import ButtonsSplash from "../component/buttonStyle";
 
 function SplashPage() {
-
-return (
-    
-    <div style={{
-        backgroundColor:"#e0fdf8",
+  return (
+    <div
+      style={{
         backgroundImage: `url(${background})`,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
@@ -22,9 +20,7 @@ return (
       </div>
       <nav></nav>
       <div className="flex">
-      <Link to="/">
         <ButtonsSplash>¡Empieza a Donar+!</ButtonsSplash>
-      </Link>
       </div>
     </div>
   );
