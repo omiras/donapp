a90007f64c9b9119d7d07544bbdb32914d8743f3
Cynox336 <cynox336@gmail.com>
2023-06-28 13:50:56 +0200

    Update button style 13:50 28/06

--- src/js/views/SplashPage.jsx
+++ src/js/views/SplashPage.jsx
@@ -1,32 +1,29 @@
 import React from "react";
-import background from "../../assets/img/dona+_splash.png"
-
-
-
+import background from "../../assets/img/dona+_splash.png";
+import ButtonsSplash from "../component/buttonStyle";
 
 function SplashPage() {
-
-
-
-
-
-    return (
-    
-    <div style={{
+  return (
+    <div
+      style={{
         backgroundImage: `url(${background})`,
         backgroundSize: "cover",
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
         minHeight: "100vh",
-        }} className="flex justify-around items-end"> 
-        {/* <img></img> */}
-            <div className="flex">
-                <button className="btn">¿Quienes somos?</button>
-                <nav></nav>
-                <button className="btn">¡Empieza a Donar+!</button>
-            </div>
+      }}
+      className="flex justify-around items-end"
+    >
+      {/* <img></img> */}
+      <div className="flex">
+        <ButtonsSplash>¿Quienes somos?</ButtonsSplash>
+      </div>
+      <nav></nav>
+      <div className="flex">
+        <ButtonsSplash>¡Empieza a Donar+!</ButtonsSplash>
+      </div>
     </div>
-    
-)}
+  );
+}
 
 export default SplashPage;
