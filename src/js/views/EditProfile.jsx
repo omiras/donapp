5e0ffd9262207a8fdd9060d2c41af69c16bd1ce7
Oscar Miras <oscar.miras@gmail.com>
2023-06-29 20:49:27 +0200

    Edit Profile

--- src/js/views/EditProfile.jsx
+++ src/js/views/EditProfile.jsx
@@ -1,30 +1,50 @@
 import { useContext } from "react";
 import { Context } from "../store/appContext";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function EditProfile() {
   const { store, actions } = useContext(Context);
-  const [name, setName] = useState(store.user.full_name);
-  const [city, setCity] = useState(store.user.city || '');
-  const [avatar, setAvatar] = useState(store.user.avatar_url);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    await actions.editProfile(data);
+  };
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="flow flex flex-col place-items-center w-full h-screen justify-center "
+      className="flow flex flex-col place-items-center h-screen justify-center "
     >
       <div className="w-full">
-        <div className="chat-image avatar w-full ">
-          <div className="w-40 rounded-full mx-auto">
-            <img src={store.user.avatar_url} />
-          </div>
-        </div>
+        <img
+          src={watch("avatar_url") || store.user.avatar_url}
+          alt=""
+          className="w-36 h-36 m-auto aspect-square object-cover"
+        />
       </div>
       <div className="edit-avatar flex flex-col justify-center">
         <h2>{watch("full_name") || store.user.full_name}</h2>
         <h4>{watch("city") || store.user.city}</h4>
       </div>
-
+      <div className="flex flex-col">
+        <label className="label">Image url</label>
+        <input
+          {...register("avatar_url", { required: "Campo requerido" })}
+          type="text"
+          defaultValue={store.user.avatar_url}
+          className="input"
+        />
+        {errors.avatar_url && (
+          <span className="text-red-500">{errors.avatar_url.message}</span>
+        )}
+      </div>
       <div className="flex flex-col">
         <label className="label">Name</label>
         <input
