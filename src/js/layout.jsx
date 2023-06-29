160089a0fd874fffa6b8218441653527f91ebc80
Oscar Miras <oscar.miras@gmail.com>
2023-06-29 19:42:00 +0200

    fix spalsh?

--- src/js/layout.jsx
+++ src/js/layout.jsx
@@ -34,8 +34,7 @@ const Layout = () => {
     checkIfUserIsLoggedIn();
   }, []);
 
-  const showNavbar = location.pathname !== "/splash";
-
+  const showNavbar = location.pathname.includes("splash");
   return (
     <BrowserRouter>
       <div className="flex flex-col w-full place-items-center">
